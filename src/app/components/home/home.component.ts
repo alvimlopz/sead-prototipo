079629684462c28a37a4020db01739ec4e6a709f
alvaro rodrigues <alvim.lopez@gmail.com>
2024-03-02 09:59:42 -0300

    Organizando o codigo

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ListTable } from '../../models/list-table';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +13,49 @@ export class HomeComponent {
 
  data: ListTable[] | undefined;
 
+ vagas: any[] = [
+  {
+    cargo: 'Analista de Gest. Governamental',
+    especialidade: 'Téc. da Informação',
+    ampla: 30,
+    pcd: 2,
+    total: 32,
+    amplaCR: 32,
+    pcdCR: 2,
+    totalCR: 34
+  },
+  {
+    cargo: 'Analista de Gest. Governamental',
+    especialidade: 'Engenharia Civil',
+    ampla: 40,
+    pcd: 4,
+    total: 44,
+    amplaCR: 21,
+    pcdCR: 4,
+    totalCR: 25
+  },
+  {
+    cargo: 'Gestor Governamental',
+    especialidade: 'Direito',
+    ampla: 35,
+    pcd: 1,
+    total: 36,
+    amplaCR: 15,
+    pcdCR: 1,
+    totalCR: 16
+  }
+];
 
+cols: any[] = [
+  { field: 'cargo', header: 'Cargo' },
+  { field: 'especialidade', header: 'Especialidade' },
+  { field: 'ampla', header: 'Ampla' },
+  { field: 'pcd', header: 'PCD' },
+  { field: 'total', header: 'Total' },
+  { field: 'amplaCR', header: 'Ampla (CR)' },
+  { field: 'pcdCR', header: 'PCD (CR)' },
+  { field: 'totalCR', header: 'Total (CR)' }
+];
 
 
 
@@ -22,9 +65,11 @@ dataHomologacao: any;
 validade: any;
   cars!: any[];
 
-  constructor() { }
+  constructor(private primengConfig: PrimeNGConfig) { }
 
   ngOnInit() {
+    this.primengConfig.ripple = true;
+
     this.cars = [
       { cargo: 'Analista de TI', especialidade: 'Desenvolvimento Web' },
       { cargo: 'Engenheiro Civil', especialidade: 'Construção Civil' },
